558d80ef97162a2c281b5281088f8d170d4d9edb
alexm98 <munteanu.c.alex@gmail.com>
2018-04-14 18:08:18 +0300

    Created main layout, login/register pages, views are i in the src folder..

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php require_once './vendor/autoload.php'; ?>
-<html>
-    <?php require './src/header.php'; ?>
-
-    <body>
-        <script type="text/javascript">
-            $(document).ready(function(){
-                $("#msgid").html("Hello World!");
-                $('#msgid').fadeIn('Slow');
-            });
-        </script>
-        <div id="msgid">
-        </div>
-    </body>
-</html>
+<?php header('Location: ./src/index.php'); ?>
---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php require_once '../vendor/autoload.php'; ?>
+
+<html>
+    <?php require_once './header.php'; ?>
+        <div class="index-body">
+            <center><h1>Index</h1></center>
+        </div>
+    </body>
+</html>
