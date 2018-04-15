7e662d749a9d04e99269643e08e605431da2ca7d
alexm98 <munteanu.c.alex@gmail.com>
2018-04-15 10:26:12 +0300

    Removed everything, starting from scratch with Laravel

--- src/index.php
+++
@@ -1,9 +0,0 @@
-<?php require_once '../vendor/autoload.php'; ?>
-
-<html>
-    <?php require_once './header.php'; ?>
-        <div class="index-body">
-            <center><h1>Index</h1></center>
-        </div>
-    </body>
-</html>
