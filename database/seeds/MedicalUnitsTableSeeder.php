c9bc6dbf17ed9967633f48ae6b951004084ee189
Ioan Samuilă <ioan.samuila73@e-uvt.ro>
2018-05-08 12:00:32 +0300

    Modificare Medical Units
    Adăugat în  Stomadmin/database/seeds/MedicalUnitsTableSeeder.php câteva clinici

--- database/seeds/MedicalUnitsTableSeeder.php
+++ database/seeds/MedicalUnitsTableSeeder.php
@@ -17,5 +17,20 @@ class MedicalUnitsTableSeeder extends Seeder
             'address' => 'Popa Șapca Nr. 8',
             'phone' => '+40 732 421 324'
         ]);
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Molarul',
+            'address' => 'Dunării Nr. 27',
+            'phone' => '+40 744 456 123'
+        ]);
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Radiologie Dentară',
+            'address' => 'Primăverii Nr. 14',
+            'phone' => '+40 766 555 222'
+        ]);
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Zâna Măseluță',
+            'address' => 'Sorin Titel Nr. 17',
+            'phone' => '+40 711 654 987'
+        ]);
     }
 }
