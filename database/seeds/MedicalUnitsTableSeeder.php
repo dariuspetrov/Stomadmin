b8cb68926bb33b5c0baab93b8faf42c26eff44fa
dariuspetrov <dariuspetrov@gmail.com>
2018-05-09 14:17:38 +0300

    Updated Records Medical Units

--- database/seeds/MedicalUnitsTableSeeder.php
+++ database/seeds/MedicalUnitsTableSeeder.php
@@ -12,7 +12,6 @@ class MedicalUnitsTableSeeder extends Seeder
     public function run()
     {
         DB::table('medical_units')->insert([
-            'unit_id' => 1,
             'name' => 'Clinica Ortodontie',
             'address' => 'Popa Șapca Nr. 8',
             'phone' => '+40 732 421 324'
@@ -34,6 +33,42 @@ class MedicalUnitsTableSeeder extends Seeder
             'name' => 'Clinica Zâna Măseluță',
             'address' => 'Sorin Titel Nr. 17',
             'phone' => '+40 711 654 987'
+        ]);
+		
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Ioan Vereș',
+            'address' => 'Spl Independenței Nr. 42',
+            'phone' => '+40 711 262 732'
+        ]);
+
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Dr. Vasile Zohoran',
+            'address' => 'Apicultorilor Nr. 32',
+            'phone' => '+40 721 384 897'
+        ]);
+
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica de Urgente Maxilo-Faciale',
+            'address' => 'Take Ionescu Nr. 2',
+            'phone' => '+40 731 497 222'
+        ]);
+
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Județeană Timișoara',
+            'address' => 'Republicii Nr. 43',
+            'phone' => '+40 731 444 673'
+        ]);
+
+		DB::table('medical_units')->insert([
+            'name' => 'Clinica Dr. Igor Aerodromovici',
+            'address' => 'Aviatorilor Nr. 34',
+            'phone' => '+40 737 747 787'
+        ]);
+
+		DB::table('medical_units')->insert([
+            'name' => 'Cabinetul Adrian Avian',
+            'address' => 'Aeroportului Nr. 43',
+            'phone' => '+40 777 320 380'
         ]);
     }
 }
