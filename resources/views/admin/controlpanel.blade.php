1ed419edcc4c3d3247b7693f0853688895c604c5
dariuspetrov <dariuspetrov@gmail.com>
2018-05-19 18:55:54 +0300

    Added admin panel icons

--- resources/views/admin/controlpanel.blade.php
+++ resources/views/admin/controlpanel.blade.php
@@ -21,21 +21,27 @@
                             </div>
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/users/create') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="fas fa-user-plus"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">Add users</h3>
                                 </a>
                             </div>
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/appointments') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="far fa-calendar-check"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">View Appointments</h3>
                                 </a>
                             </div>
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/appointments/create') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="far fa-calendar-plus"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">Create Appointments</h3>
                                 </a>
@@ -45,28 +51,36 @@
                         <div class="row">
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/medicalunits') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="fas fa-hospital-alt"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">View Medical Units</h3>
                                 </a>
                             </div>
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/medicalunits/create') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="fas fa-file-medical"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">Create Medical Units</h3>
                                 </a>
                             </div>
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/interventions/') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="fas fa-syringe"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">View Interventions</h3>
                                 </a>
                             </div>
                             <div class="col-md-3 panel-item">
                                 <a href="{{ url('/interventions/create') }}" >
-                                    <div class="panel-item-image"></div>
+                                    <div class="panel-item-image">
+                                    	<i class="fas fa-file-medical-alt"></i>
+                                    </div>
                                     <hr>
                                     <h3 class="text-center">Create Interventions</h3>
                                 </a>
@@ -79,3 +93,4 @@
     </div>
     </div>
 @stop
+
