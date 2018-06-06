ae10859e0332f6071b884774f655130fedd7926f
alexm98 <munteanu.c.alex@gmail.com>
2018-06-06 15:21:57 +0300

    Merge pull request #31 from dariuspetrov/master
    Added record into Reference Letter

--- database/seeds/ReferenceLetterTableSeeder.php
+++ database/seeds/ReferenceLetterTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Carbon\Carbon;
 
 class ReferenceLetterTableSeeder extends Seeder
 {
@@ -11,6 +12,14 @@ class ReferenceLetterTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('refference_letters')->insert([
+            'doctor_id' => 3,
+            'pacient_id' => 7,
+            'intervention_id' => 2,
+            'unit_id' => 2,
+            'diagnostic' => 'Gingivită acută',
+            'medicine' => 'Algocalmin',
+            'date' => Carbon::now()->format('Y-m-d H:i:s'),
+        ]);
     }
 }
